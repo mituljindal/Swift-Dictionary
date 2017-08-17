80745792d56da4e99cfe12469925fd44fc2eeb68
mitul jindal <mituljindal@gmail.com>
2017-08-17 18:22:04 +0530

    setup basic UI

--- Dictionary/DictionaryViewController.swift
+++ Dictionary/DictionaryViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class DictionaryViewController: UIViewController {
     
+    @IBOutlet weak var searchField: UITextField!
 
+    @IBOutlet weak var meaningField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func search(_ sender: Any) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
